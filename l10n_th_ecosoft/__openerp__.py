25e85fb4bdb8102a9e47dde5c00f2b23593bf236
kittiu <kittiu@kittiu>
2016-01-02 14:05:42 +0700

    [ADD] l10n_th_ecosoft

---
+++ l10n_th_ecosoft/__openerp__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2011 Smartmode LTD (<http://www.smartmode.co.uk>).
+
+{
+    'name': 'Thailand - Chart of account (ECOSOFT)',
+    'version': '1.0',
+    'category': 'Localization/Account Charts',
+    'description': """
+    """,
+    'author': 'Kitti U. (Ecosoft)',
+    'website': 'http://ecosoft.co.th',
+    'depends': ['account'],
+    'data': [
+        'data/account_chart_template.xml',
+        'data/account.account.template.csv',
+        'data/account.chart.template.csv',
+        'data/account.account.tag.csv',
+        'data/account.tax.template.csv',
+        'data/account_chart_template.yml',
+    ],
+    'demo' : [],
+    'installable': True,
+}
